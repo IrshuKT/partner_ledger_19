3633687ba39c414635b34a8ddc06d2611c5e307a
ktscomputer <contact.ktcs@gmail.com>
2025-10-22 10:50:01 +0530

    inital updates

---
+++ __manifest__.py
@@ -0,0 +1,34 @@
+{
+    'name': "Partner Ledger",
+
+    'summary': 'Partner ledger quick view with running balance',
+    'description': """
+    """,
+
+    'author': "Irshad K T",
+    'website': "www.linkedin.com/in/irshadkt",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Accounting',
+    'version': '0.4',
+    'license': 'LGPL-3',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'account'],
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'templates/partner_ledger_template.xml',
+        'templates/partner_leger_action.xml',
+        'views/partner_ledger.xml',
+        'wizard/partner_ledger_wizard.xml',
+        #'wizard/partner_ledger_wizard.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
+
